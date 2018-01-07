34e4263c876d6af7583f79c513aff756703f888a
Egecan Yilmaz <egecanyilmaz@gmail.com>
2018-01-07 14:29:30 +1100

    upload1 and source options implemented

--- src/utils/DataService.js
+++ src/utils/DataService.js
@@ -10,8 +10,8 @@ export function showInvalidFileTypeMessage(){
   window.alert("Tried to upload invalid filetype. Only " + allowedFileTypes + " is allowed");
 }
 
-export function getData(riskfree, stocks) {
-  const url = `/ef?source=google&symbols=` + stocks +`&riskfree=` + riskfree
+export function getData(riskfree, stocks, source) {
+  const url = `/ef?source=` + source + `&symbols=` + stocks +`&riskfree=` + riskfree
   return fetch(url)
       .then(handleErrors)
       .then(response =>{
@@ -27,13 +27,14 @@ function handleErrors(response) {
   return response;
 }
 
-export function fileUpload(file, riskfree){
+export function fileUpload(file, riskfree, upload1){
   const formData = new FormData();
   formData.append('the_file', file)
   formData.append('riskfree', riskfree)
 
+  const req = upload1 ? `/upload1` : `/upload`
   //return  post(url, formData,config)
-  return fetch(`/upload`, {
+  return fetch(req, {
     method: 'post',
     body: formData
   })
